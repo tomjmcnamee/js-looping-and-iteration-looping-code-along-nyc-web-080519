dad9f2d4370b722924c026dd4d7ac28e1e00a08c
tomjmcnamee <tom@tomjmcnamee.com>
2019-09-15 10:53:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names_arr, event_name) {
+  const new_arr = [];
+  for (let i = 0; i < names_arr.length; i++){
+    new_arr.push(`Thank you, ${names_arr[i]}, for the wonderful ${event_name} gift!`);
+  }
+  return new_arr;
+}
+
+
+function countdown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
